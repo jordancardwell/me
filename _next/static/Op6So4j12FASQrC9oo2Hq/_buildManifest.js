f29ec5632c55f346941709c18f2f1df3f5d92ffa
jordancardwell <JordanCardwell@gmail.com>
2022-04-20 15:40:11 +0000

    Deploying to gh-pages from @ jordancardwell/me@26fbda02cab5e37f036083e420458a6c01ff96c6 🚀

--- _next/static/Op6So4j12FASQrC9oo2Hq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/160-938d43b0bc8007cd.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-0af7265aa7f7f3e1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
